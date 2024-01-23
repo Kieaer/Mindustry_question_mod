cc9b6dfa8c27542b006e212994f5ef6e23c10883
Gureumi <cloud9350@naver.com>
2024-01-23 14:59:29 +0900

    Add missing ping sound

---
+++ src/gureumi/Main.java
@@ -0,0 +1,15 @@
+package gureumi;
+
+import arc.util.Log;
+import mindustry.mod.*;
+
+public class Main extends Mod{
+    public Main() {
+        Log.info("unlock sound is now lol missing ping sound.");
+    }
+
+    @Override
+    public void loadContent(){
+
+    }
+}
